1a1630ce7b00b8e643f6243b53d7516cde4852b0
AviramGelblum <avgelb@gmail.com>
2017-08-31 19:29:29 +0300

    Finished drawing methods: added pcolormesh representation of tile density.

--- BoxesSimulation.py
+++ BoxesSimulation.py
@@ -6,6 +6,9 @@ from BoxAnalysis import BoxAnalysis, DistributionResults
 import math
 import misc
 import movie
+from path import MotionPath
+import matplotlib
+
 
 class TiledGridSimulation:
     def __init__(self, video_number, pickle_density_motion_file_name, tile_scale=1,
@@ -129,9 +132,8 @@ class TiledGridSimulation:
                 drawn = np.random.random()
             self.path.append(self.path[-1] + misc.sign(drawn-0.5)*add_dict[direction])
 
-        l = len(relevant_distribution.length_distribution)
         drawn = np.random.random()
-        index = math.floor(drawn/(1/l))
+        index = math.floor(drawn/(1/len(relevant_distribution.length_distribution)))
         self.length_results.append(relevant_distribution.length_distribution[index])
         self.time_results.append(relevant_distribution.time_distribution[index])
 
@@ -140,8 +142,7 @@ class TiledGridSimulation:
             return True, 'InBounds'
         elif self.path[-1].x > self.actual_x_range[1]:
             return True, 'Front'
-        elif self.path[-1].y > self.actual_y_range[1] or \
-             self.path[-1].y < self.actual_y_range[0]:
+        elif self.path[-1].y > self.actual_y_range[1] or self.path[-1].y < self.actual_y_range[0]:
             return True, 'Sides'
         return False, 'Continue'
 
@@ -199,31 +200,81 @@ class SimulationResults:
                                  self.final_out[key], self.scale[key], self.number_of_cubes[key],
                                  self.video_number[key])
 
-    def draw_results(self, cfg: configuration.Configuration,kwargsdict=None):
-        if kwargsdict is None:
-            kwargsdict=dict()
-        draw_obj = movie.Movie()
-        draw_obj.background([(cfg, None)])
-        self.draw(**kwargsdict)
+    def draw_results(self, cfg: configuration.Configuration, save=False, save_location=None,
+                     background_kwargs_dict=None, simulation_kwargs_dict=None):
+        assert len(self) == 1, 'must be a single simulation'
+        if background_kwargs_dict is None:
+            background_kwargs_dict = {'CubeColor': misc.RGB_255to1((139, 101, 8)), 'PathAlpha':
+                                      0.2, 'ShadeAlpha': 0.4, 'CubeAlpha': 0.3}
+        drawing_obj = movie.Movie()
+        self.draw(drawing_obj, simulation_kwargs_dict)
+        drawing_obj.ax.set_ylim(cfg.y_range[0]-0.05, cfg.y_range[1]+0.05)
+        drawing_obj.background([(cfg, background_kwargs_dict)])
+        if save:
+            if save_location is None:
+                raise ValueError('save_location must be provided')
+            drawing_obj.save_figure(save_location)
+        else:
+            drawing_obj.just_draw()
 
-    def draw(self,color='black',cmap=''):
+    def draw(self, drawing_obj, simulation_kwargs_dict):
         """Draw one iteration"""
-        assert len(self)==1
-        if len(self)>1:
-            for i in range(0,len(self)):
+        assert len(self) == 1, 'must be a single simulation object'
+        current_sim = self[0]
+        # if len(self) > 1:
+        #     for i in range(0, len(self)):
+        #         placeholder = 1
+        default_grid_keys = ['cmap', 'cmap_alpha']
+        default_grid_values = ['OrRd', 0.8]
+        grid_dict = dict(zip(default_grid_keys, default_grid_values))
+        default_simulation_path_keys = ['PathColor', 'alpha']
+        default_simulation_path_values = ['green', 1]
+        simulation_path_dict = dict(zip(default_simulation_path_keys, default_simulation_path_values))
+        if simulation_kwargs_dict is not None:
+            for key in set(default_grid_keys).intersection(simulation_kwargs_dict.keys()):
+                grid_dict[key] = simulation_kwargs_dict[key]
+            for key in set(default_simulation_path_keys).intersection(simulation_kwargs_dict.keys()):
+                simulation_path_dict[key] = simulation_kwargs_dict[key]
+
+        current_path = MotionPath(current_sim.path[0])
+        current_path.draw(drawing_obj.ax, {'color': simulation_path_dict['PathColor'],
+                                           'alpha': simulation_path_dict['alpha']})
+
+        tilefilename = 'Tiled Grids/' + self.video_number[0] + '_grid_scale_' + str(self.scale[0]) \
+                       + '.pickle'
+        with open(tilefilename, 'rb') as handle:
+            tiled_grid = pickle.load(handle)
+
+        x_unique = np.unique([tup[0].x for tup in tiled_grid])
+        y_unique = np.unique([tup[0].y for tup in tiled_grid])
+
+        [xmesh, ymesh] = np.meshgrid(x_unique[slice(0, len(x_unique), 2)],
+                                     y_unique[slice(0, len(y_unique), 2)])
+        zmesh = np.empty(xmesh.shape)
+
+        for i in range(0, xmesh.shape[0]):
+            for j in range(0, xmesh.shape[1]):
+                gr_generator = (k[0] for k in enumerate(tiled_grid) if
+                                k[1][0].dist(P(xmesh[i, j], ymesh[i, j])) < 10 ** -8)
+                zmesh[i, j] = tiled_grid[next(gr_generator)][1]
+
+        pcm = drawing_obj.ax.pcolormesh(xmesh, ymesh, zmesh, **{'cmap': grid_dict['cmap'], 'alpha':
+                                        grid_dict['cmap_alpha']})
+        drawing_obj.fig.colorbar(pcm)
+
 
 
 #########################################################################
 
 
 if __name__ == "__main__":
-    scale_list = [8, 10, 15] #1, 2, 4, 6
+    scale_list = [1, 2, 4, 6, 8, 10, 15]
     number_of_iterations = 50
     loadloc = 'middle'
     pickle_file_name = 'Pickle Files/ExperimentalBoxDistribution' + loadloc + '.pickle'
     results_pickle_file_name = 'Pickle Files/SimulationResults_' + str(
         number_of_iterations)+'iterations'
-    cube_densities = [100, 200, 225, 250, 275, 300]  #
+    cube_densities = [100, 200, 225, 250, 275, 300]
     for tilescale in scale_list:
         for cube_density in cube_densities:
             for video_number in misc.file_names_by_density(cube_density):
