ff65f65a532991f0f4e5a381ebdbb971ec54f08f
AviramGelblum <avgelb@gmail.com>
2017-12-12 19:18:40 +0200

    routine script changes

--- RunSimulation.py
+++ RunSimulation.py
@@ -16,7 +16,8 @@ def sim_func(tilescale):
     # rolling = True
     # sigma = 0.35
     # persistence_distance = 6
-    pickle_file_name = 'Pickle Files/ExperimentalBoxDistribution' + loadloc + '.pickle'
+    pickle_file_name = 'Pickle Files/ExperimentalBoxDistribution' + loadloc \
+                       + ' correct cube scaling 0p8' + '.pickle'
     # distribution_results_filename_root = 'Pickle Files/SimulationBoxDistribution_' + loadloc + \
     #                                      '_rolling_' + str(rolling) + '_sigma_' + str(
     #                                      sigma).replace('.', 'p') + '_PersistenceDistance_' \
@@ -25,28 +26,29 @@ def sim_func(tilescale):
     #     number_of_iterations)+'iterations'
     # results_pickle_file_name = 'Pickle Files/QuenchedSimulationResults_' + str(
     #     number_of_iterations)+'iterations'
-    cube_densities = [100, 150, 200, 225, 250, 275, 300, 400]
+    cube_densities = [100, 200, 225, 250, 275, 300]  # ,150, 400]
     # number_of_mazes_per_density = 30
     # bias_values = ('constant', 0.75)
     # bias_values = ('hooke', 0.8, 10, 0.3)
     # seed_lists = [[seed for seed in misc.yield_rand(number_of_mazes_per_density)] for i in range(
     #              len(cube_densities))]
     for cube_density in cube_densities:
-        for seed in misc.seed_from_txt_files(cube_density):
-        # for video_number in misc.file_names_by_density(cube_density):
-        #       print('scale = ' + str(tilescale))
+        #for seed in misc.seed_from_txt_files(cube_density):
+        for video_number in misc.file_names_by_density(cube_density):
+            #print('scale = ' + str(tilescale))
 
             for iteration in range(1, number_of_iterations+1):
-                #print('scale = ' + str(tilescale) + ',seed = ' + seed + ',iter = '
-                #      + str(iteration))
-                print('scale = ' + str(tilescale) + ', seed = ' + seed + ', iteration = ' +
-                      str(iteration))
+                print('scale = ' + str(tilescale) + ',video number = ' + video_number +
+                      ',iteration = ' + str(iteration))
+                # print('scale = ' + str(tilescale) + ', seed = ' + seed + ', iteration = ' +
+                #      str(iteration))
                 # pickle_file_name = distribution_results_filename_root + str(tilescale) + \
                 #                    '_stucking.pickle'
-                # sim = TiledGridSimulation(pickle_file_name, video_number=video_number,
-                #                           tile_scale=tilescale, is_stuckable=True)
-                sim = TiledGridSimulation(pickle_file_name, seed=int(seed), num_stones=cube_density,
+                sim = TiledGridSimulation(pickle_file_name, video_number=video_number,
                                           tile_scale=tilescale, is_stuckable=False)
+                # sim = TiledGridSimulation(pickle_file_name, seed=int(seed),
+                # num_stones=cube_density,
+                #                          tile_scale=tilescale, is_stuckable=False)
                 # sim = QuenchedGridSimulation(pickle_file_name, video_number=video_number,
                 #                               tile_scale=tilescale)
                 # sim = QuenchedGridTrailBiasSimulationLoopStuck(pickle_file_name, bias_values,
@@ -54,20 +56,29 @@ def sim_func(tilescale):
                 #                                    tile_scale=tilescale)
                 load_path, load_trajectory_length, load_time, where_out = sim.run_simulation()
                 try:
+                    # ResultsObject.append(SimulationResults(load_path, load_trajectory_length,
+                    #                                        load_time, where_out, tilescale,
+                    #                                        cube_density, seed=int(seed)))
                     ResultsObject.append(SimulationResults(load_path, load_trajectory_length,
                                                            load_time, where_out, tilescale,
-                                                           cube_density, seed=int(seed)))
+                                                           cube_density, video_number=video_number))
                 except NameError as e:
                     if e.args[0][16:29] == 'ResultsObject':
+                        # ResultsObject = SimulationResults(load_path, load_trajectory_length,
+                        #                                   load_time, where_out, tilescale,
+                        #                                   cube_density, seed=int(seed))
                         ResultsObject = SimulationResults(load_path, load_trajectory_length,
                                                           load_time, where_out, tilescale,
-                                                          cube_density, seed=int(seed))
+                                                          cube_density, video_number=video_number)
                     else:
                         raise
                 except AttributeError:
+                    # ResultsObject = SimulationResults(load_path, load_trajectory_length,
+                    #                                   load_time, where_out, tilescale,
+                    #                                   cube_density, seed=int(seed))
                     ResultsObject = SimulationResults(load_path, load_trajectory_length,
                                                       load_time, where_out, tilescale,
-                                                      cube_density, seed=int(seed))
+                                                      cube_density, video_number=video_number)
     # results_pickle_file_name = 'Pickle Files/SimulationResults_Scale_' + str(tilescale) + \
     #                            '_iterations_' + str(number_of_iterations) + '.pickle'
     # results_pickle_file_name = 'Pickle Files/QuenchedSimulationResults_Scale_' + str(tilescale) \
@@ -81,7 +92,10 @@ def sim_func(tilescale):
     #                             + str(sigma).replace('.', 'p') + '_PersistenceDistance_' \
     #                             + str(persistence_distance).replace('.', 'p') + '_iterations_'\
     #                             + str(number_of_iterations) + '.pickle'
-    results_pickle_file_name = 'Pickle Files/SimulationResults_Simulated_Cubes_Scale_' \
+    # results_pickle_file_name = 'Pickle Files/SimulationResults_Simulated_Cubes_Scale_' \
+    #                            + str(tilescale) + '_iterations_' + str(number_of_iterations) \
+    #                            + '.pickle'
+    results_pickle_file_name = 'Pickle Files/SimulationResults_correct_cube_size_Scale_' \
                                + str(tilescale) + '_iterations_' + str(number_of_iterations) \
                                + '.pickle'
     with open(results_pickle_file_name, 'wb') as handle:
